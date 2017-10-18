c303ca86ef9faab204cc5bac15f93a1eef13e32e
ordemo <o.livierdelobre@gmail.com>
2017-10-18 10:01:56 +0200

    cleanup tree tests

--- src/app/tree/tree.e2e.ts
+++ src/app/tree/tree.e2e.ts
@@ -839,133 +839,35 @@ describe('Tree', () => {
     expect(await element(by.id('manage-planned-unit-13030')).getAttribute('style')).toBe('opacity: 0.33;');
   });
 
-  /***********************************************************************************************************
-   * 
-   * 
-   * Delete Unit
-   * 
-   * 
-   ************************************************************************************************************/
-  it('should delete unit from search',  async () => {
-    // Delete created unit
-    await browser.get('/#/recherche');
-    let sigleInput = await element(by.id('input-sigle'));
-    await sigleInput.clear();
-    await sigleInput.sendKeys('iDevelOP2');
-    await element(by.id('search-button')).click();
-    await browser.sleep(1000).then(function(){ });
-    // Click on delete button
-    //let deleteButtons = await element.all(by.className('icon-delete-unit')).get(0).click();
-    let count = await element.all(by.className('icon-delete-unit')).count();
-    let deleteButtons = await element.all(by.className('icon-delete-unit'));
-    //for (let deleteButton of await element.all(by.className('icon-delete-unit'))) {
-    while (count > 0) {
-      await element.all(by.className('icon-delete-unit')).get(0).click();
-      await browser.sleep(1000).then(function(){ });
-      // Check that deletion popup is displayed
-      expect(await element(by.id('modal-delete-unit')).isDisplayed()).toBe(true);
-      // Confirm delete by clicking on the button
-      await element(by.id('btn-delete-unit-confirmation')).click();
-      await browser.sleep(1000).then(function(){ });
-      count = await element.all(by.className('icon-delete-unit')).count();
-    }
-  });
-
-
-  // beforeEach(() => {
-    // browser.get('/#/oauth2/Units.d14b30ef4d67a29b9481e2891419a5a4e41dc2ef');
-    // let usernameInput = element(by.id('username'));
-    // let usernamePassword = element(by.id('password'));
-    // usernameInput.clear();
-    // usernameInput.sendKeys('test');
-    // usernamePassword.clear();
-    // usernamePassword.sendKeys('test');
-
-    // element(by.css('button.btn-primary')).click();
+  // /***********************************************************************************************************
+  //  * 
+  //  * 
+  //  * Delete Unit
+  //  * 
+  //  * 
+  //  ************************************************************************************************************/
+  // it('should delete unit from search',  async () => {
+  //   // Delete created unit
+  //   await browser.get('/#/recherche');
+  //   let sigleInput = await element(by.id('input-sigle'));
+  //   await sigleInput.clear();
+  //   await sigleInput.sendKeys('iDevelOP2');
+  //   await element(by.id('search-button')).click();
+  //   await browser.sleep(1000).then(function(){ });
+  //   // Click on delete button
+  //   //let deleteButtons = await element.all(by.className('icon-delete-unit')).get(0).click();
+  //   let count = await element.all(by.className('icon-delete-unit')).count();
+  //   let deleteButtons = await element.all(by.className('icon-delete-unit'));
+  //   //for (let deleteButton of await element.all(by.className('icon-delete-unit'))) {
+  //   while (count > 0) {
+  //     await element.all(by.className('icon-delete-unit')).get(0).click();
+  //     await browser.sleep(1000).then(function(){ });
+  //     // Check that deletion popup is displayed
+  //     expect(await element(by.id('modal-delete-unit')).isDisplayed()).toBe(true);
+  //     // Confirm delete by clicking on the button
+  //     await element(by.id('btn-delete-unit-confirmation')).click();
+  //     await browser.sleep(1000).then(function(){ });
+  //     count = await element.all(by.className('icon-delete-unit')).count();
+  //   }
   // });
-
-  /*
-  it('should expand tree and update unit', () => {
-    browser.get('/#/arbre');
-    browser.sleep(1000).then(function(){ });
-
-    let subject = browser.getTitle();
-    let result  = 'Gestion des unités';
-    expect(subject).toEqual(result);
-
-    let h1 = element(by.tagName('h1')).getText();
-    result  = 'ARBRE DES UNITÉS';
-    expect(h1).toEqual(result);
-
-    //test click on a unit
-    //let chevron = element(by.css('div.chevron-cell'));
-    let chevron = element(by.id('chevron-cell-10000'));
-    chevron.click();
-    browser.sleep(1000).then(function(){ });
-
-    chevron = element(by.id('chevron-cell-12635'));
-    chevron.click();
-    browser.sleep(1000).then(function(){ });
-
-    chevron = element(by.id('chevron-cell-13028'));
-    chevron.click();
-    browser.sleep(1000).then(function(){ });
-
-    //test if new labels have been loaded and if value is as expected
-    let labels = element.all(by.css('div.label-cell'));
-    expect(labels.count()).toBeGreaterThan(4);
-    let labelIDEVELOP = element(by.id('label-cell-13030'));
-    expect(labelIDEVELOP).not.toBeNull();
-    expect(labelIDEVELOP.getText()).toContain('SI - Développement');
-
-    //click on label to open modal
-    labelIDEVELOP.click();
-    browser.sleep(1000).then(function(){ });
-    expect(element(by.id('modal-update-unit')).isDisplayed()).toBe(true);
-
-    //text responsible auto complete
-    let responsibleInput = element(by.css('#update-unit-responsible input'));
-    responsibleInput.clear();
-    browser.sleep(1000).then(function(){ });
-    responsibleInput.sendKeys('DELOBRE');
-    browser.sleep(2000).then(function(){ });
-
-    let responsibleList = element(by.css('#update-unit-responsible ul.ui-autocomplete-items'));
-    expect(responsibleList.isDisplayed()).toBe(true);
-    let responsibleItem = element(by.css('#update-unit-responsible > span > div > ul.ui-autocomplete-items > li > span'));
-    expect(responsibleItem.getText()).toContain('DELOBRE OLIVIER ROMAIN');
-    responsibleItem.click();
-
-    //text room auto complete
-    let roomInput = element(by.css('#update-unit-room input'));
-    roomInput.clear();
-    browser.sleep(1000).then(function(){ });
-    roomInput.sendKeys('INN 0');
-    browser.sleep(2000).then(function(){ });
-
-    let roomList = element(by.css('#update-unit-room ul.ui-autocomplete-items'));
-    expect(roomList.isDisplayed()).toBe(true);
-    let roomItem = element(by.css('#update-unit-room > span > div > ul.ui-autocomplete-items > li > span'));
-    expect(roomItem.getText()).toContain('INN 0');
-    roomItem.click();
-
-    //validate changes
-    element(by.id('btn-submit-unit-update')).click();
-    browser.sleep(1000).then(function(){ });
-    expect(element(by.id('modal-update-unit')).isDisplayed()).toBe(false);
-
-    //reopen to check values
-    labelIDEVELOP.click();
-    browser.sleep(1000).then(function(){ });
-    expect(responsibleInput.getAttribute('value')).toBe('DELOBRE OLIVIER ROMAIN');
-    expect(roomInput.getAttribute('value')).toBe('INN 027');
-    expect(element(by.id('update-unit-address')).getAttribute('value')).toBe('EPFL SI IDEVELOP, INN 027 (Bâtiment INN), Station 14, CH-1015 Lausanne');
-
-    //close modal
-    element(by.id('btn-close-unit-update')).click();
-    browser.sleep(1000).then(function(){ });
-    expect(element(by.id('modal-update-unit')).isDisplayed()).toBe(false);
-  });
-  */
-
 });
